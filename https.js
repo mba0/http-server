e18ca3776c10eddcda5fa1136f11d085644161b3
MBA <148908741+mba0@users.noreply.github.com>
2023-10-24 22:46:30 +0200

    Update https.js

--- https.js
+++ https.js
@@ -1,41 +1,14 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 const https = require('https');
-const url = require('url');
 const path = require('path');
 const fs = require('fs');
-var pk = fs.readFileSync('./key.pem');
-var pc = fs.readFileSync('./cert.pem');
-var optss = { key: pk, cert: pc };
-var blacklist = new Map();
+var sec = { key: fs.readFileSync('./key.pem'), cert: fs.readFileSync('./cert.pem') };
+const port = process.argv[2] || 65536;
+const addr = "1.1.1.1";
 
-const port = process.argv[2] || 17091;
-require('events').EventEmitter.prototype._maxListeners = 100;
+var packet = `server|${addr}\nport|${port}\ntype|1\n#maint|Server\n\n\nbeta_server|127.0.0.1\nbeta_port|17091\nbeta_type|1\nbeta2_server|127.0.0.1\nbeta2_port|${port}\nbeta2_type|1\nmeta|${Math.floor(Date.now() / 1000)}\nRTENDMARKERBS1001`;
 
-var packet = `server|127.0.0.1\nport|4141\ntype|1\n#maint|Server\n\n\nbeta_server|127.0.0.1\nbeta_port|17091\nbeta_type|1\nbeta2_server|127.0.0.1\nbeta2_port|17099\nbeta2_type|1\nmeta|${Math.floor(Date.now() / 1000)}\nRTENDMARKERBS1001`;
-
-var server = https.createServer(optss,function (req, res) {
-  console.log(`Connection from: ${req.connection.remoteAddress} Url: ${req.url} Method: ${req.method}` );
-  const parsedUrl = url.parse(req.url);
+var server = https.createServer(sec, function (request, response) {
+  console.log(`\u001b[32mA new connection has made from: ${request.connection.remoteAddress}`);
   let pathname = `.${parsedUrl.pathname}`;
   const ext = path.parse(pathname).ext;
   const map = {
@@ -53,80 +26,50 @@ var server = https.createServer(optss,function (req, res) {
     '.doc': 'application/msword'
   };
 
-  if(req.url === "/growtopia/server_data.php")
+  if(request.url === "/growtopia/server_data.php")
   {
-    res.write(packet, function (err) {
+    response.write(packet, function (err) {
       if (err)
           console.log(err);      
   });
-  res.end();
-  res.destroy();
+  response.end();
+  response.destroy();
   }
   else
   {
-    if( req.url.indexOf("/cache") !== -1)
+    if( (request.url.indexOf("/cache") || request.url.indexOf("/0098")) !== -1)
     {
-      console.log(`Connection from: ${req.connection.remoteAddress} Url is: ${req.url}`);
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 301;
-      res.writeHead(301, {
-        Location: `https://ubistatic-a.akamaihd.net/0098/28378${req.url}`
-      }).end();
-            return;
-    }
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 404;
-        res.end(`error from loading`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });    
-
-    }
-    else if(req.url.indexOf("/0098") !== -1)
-    {    
-    console.log(`Connection from: ${req.connection.remoteAddress} Url is: ${req.url}`);
-
+      console.log(`\u001b[32mA new connection has made from: ${request.connection.remoteAddress}`);
       fs.exists(pathname, function (exist) {
-        if(!exist) {
-          // if the file is not found, return 404
-          res.statusCode = 301;
-          res.writeHead(301, {
-            Location: `https://ubistatic-a.akamaihd.net/0098/28378${req.url}`
-          }).end();
-              return;
+      if(!exist) {
+        response.statusCode = 301;
+        response.writeHead(301, {
+        Location: `https://ubistatic-a.akamaihd.net/0098/28378${request.url}`
+      }).end();
+      return;
+    }
+    fs.readFile(pathname, function(err, data) {
+      if(err){
+        response.statusCode = 404;
+        response.end(`err`);
+      }
+      else {
+        response.setHeader('Content-type', map[ext] || 'text/plain' );
+        response.end(data);
         }
-    
-        // read file from file system
-        fs.readFile(pathname, function(err, data){
-          if(err){
-            res.statusCode = 404;
-            res.end(`error`);
-          } else {
-            // if the file is found, set Content-type and send data
-            res.setHeader('Content-type', map[ext] || 'text/plain' );
-            res.end(data);
-          }
-        });
-      });  
-    }
-    else
-    {
-      res.statusCode = 301;
-      res.end();
-
-    }
+      });
+      
+    });    
   }
+  else
+  {
+    response.statusCode = 301;
+    response.end();
+  }
+}
 
 });
+
 server.listen(parseInt(port));
 
 
